4da236894752e942ec5562bdd7b83fe77fc7f5d5
rakesh <92536736+rakeshsonkar@users.noreply.github.com>
2022-01-22 21:24:09 +0530

    Add files via upload

---
+++ constant.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+define('SITE_NAME','Food Ordering Admin');
+define('FRONT_SITE_NAME','Food Ordering');
+
+define('FRONT_SITE_PATH','https://food.eshopping.world/');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/");
+
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+define('SITE_DISH_IMAGE',FRONT_SITE_PATH."media/dish/");
+
+define('SERVER_BANNER_IMAGE',SERVER_IMAGE."media/banner/");
+define('SITE_BANNER_IMAGE',FRONT_SITE_PATH."media/banner/");
+
+?>
---
+++ database.inc.php
@@ -0,0 +1,4 @@
+<?php
+date_default_timezone_set('Asia/Kolkata');
+$con=mysqli_connect('localhost','eshoppin_food','Rakesh@#9118686068','eshoppin_food');
+?>
